e5ae6f37b73d83dd07168c8b7ef6dea0d077f283
Patrick Leandro <patrickleandro34@gmail.com>
2020-11-14 16:22:04 -0300

    mudança de diaria adicionada

--- script/script.js
+++ script/script.js
@@ -1,67 +1,115 @@
 const day = ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado'];
 const date = new Date();
 const fullDate = date.getDay();
-var diaria = '';
+var diaria = 0;
 var listEntregas = document.getElementById('listEntregas');
 var entrega = document.getElementById('entrega');
 var entregas = 0;
 var total = 0;
 var motoca = document.getElementById('motoca');
 document.getElementById('dia').innerHTML = day[fullDate];
+var contador = 0;
+var alterarDiaria = 0;
 
-function testefoto() {
-    switch (motoca.value) {
-        case 'Patrick':
+function trocafoto() {
+    contador++;
+    switch (contador) {
+        case 1:
             document.getElementById('foto').src='imagens/patrick.jpg'
             break;
-        case 'Leonardo':
+        case 2:
             document.getElementById('foto').src='imagens/leo.png'
             break;
-        case 'Panco':
+        case 3:
             document.getElementById('foto').src='imagens/panco.png'
             break;
     
         default:
             document.getElementById('foto').src='imagens/download-removebg-preview.png'
+            contador = 0;
+            break;
+    
+    }
+}
+
+function mudarDiaria() {
+    alterarDiaria++;
+    switch (alterarDiaria) {
+        case 1:
+            diaria = 35;
+            document.getElementById('diaria').innerHTML = `Diária: &nbspR$${diaria},00`;
+            document.getElementById('totalEntregas').innerHTML= `Total da noite R$${diaria},00`        
+            break;
+        case 2:
+            diaria = 50;
+            document.getElementById('diaria').innerHTML = `Diária: &nbspR$${diaria},00`;
+            document.getElementById('totalEntregas').innerHTML= `Total da noite R$${diaria},00`
+        break
+        case 3:
+            diaria = 55;
+            document.getElementById('diaria').innerHTML = `Diária: &nbspR$${diaria},00`;
+            document.getElementById('totalEntregas').innerHTML= `Total da noite R$${diaria},00`
+        break
+        default:
+            diaria = 60;
+            alterarDiaria = 0;
+            document.getElementById('diaria').innerHTML = `Diária: &nbspR$${diaria},00`;
+            document.getElementById('totalEntregas').innerHTML= `Total da noite R$${diaria},00`
+            break;
+    }
+
+    
+    
+}
+
+function diariaMotoca() {
+    
+    switch (fullDate) {
+        case 0:
+            diaria = 60;
+            break;
+    
+        case 1:
+            diaria = 'Pizzaria fechada';           
+            document.getElementById('diaria').innerHTML = `Diária: &nbspR$${diaria},00`;
+            document.getElementById('totalEntregas').innerHTML= `Total da noite R$${diaria},00`
+            break;
+    
+        case 2:
+            diaria = 55;
+            document.getElementById('diaria').innerHTML = `Diária: &nbspR$${diaria},00`;
+            document.getElementById('totalEntregas').innerHTML= `Total da noite R$${diaria},00`
+            break;
+    
+        case 3:
+            diaria = 55;
+            document.getElementById('diaria').innerHTML = `Diária: &nbspR$${diaria},00`;
+            document.getElementById('totalEntregas').innerHTML= `Total da noite R$${diaria},00`           
+            break;
+    
+        case 4:
+            diaria = 55;
+            document.getElementById('diaria').innerHTML = `Diária: &nbspR$${diaria},00`;
+            document.getElementById('totalEntregas').innerHTML= `Total da noite R$${diaria},00`
+            break;
+    
+        case 5:
+            diaria = 60;
+            document.getElementById('diaria').innerHTML = `Diária: &nbspR$${diaria},00`;
+            document.getElementById('totalEntregas').innerHTML= `Total da noite R$${diaria},00`          
+            break;
+    
+        case 6:
+            diaria = 60;
+            document.getElementById('diaria').innerHTML = `Diária: &nbspR$${diaria},00`;
+            document.getElementById('totalEntregas').innerHTML= `Total da noite R$${diaria},00`            
+            break;
+    
+        default:
             break;
     }
 }
 
-switch (fullDate) {
-    case 0:
-        diaria = 60;
-        break;
-
-    case 1:
-        diaria = 'Pizzaria fechada';
-        break;
-
-    case 2:
-        diaria = 55;
-        break;
-
-    case 3:
-        diaria = 55;
-        break;
-
-    case 4:
-        diaria = 55;
-        break;
-
-    case 5:
-        diaria = 60;
-        break;
-
-    case 6:
-        diaria = 60;
-        break;
-
-    default:
-        break;
-}
-
-document.getElementById('diaria').innerHTML = `Diária: &nbspR$${diaria},00`;
-document.getElementById('totalEntregas').innerHTML= `Total da noite R$${diaria},00`
 
 
 
